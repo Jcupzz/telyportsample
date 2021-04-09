cfa1324dec8b636843563a0e4d96928f32dced16
Jcupzz <jcupzz255@gmail.com>
2021-04-10 00:36:00 +0530

    merged the error

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:telyportsample/Static/MyBottomNavigationBar.dart';
 
 import 'Pages/MyHomePage.dart';
